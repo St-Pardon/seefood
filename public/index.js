86449b157b06eac2ee499560848c3e251325b8b0
St Pardon <okc4pardon@gmail.com>
2023-03-06 21:34:02 +0100

    :hammer: updated the backend and client js

--- public/index.js
+++ public/index.js
@@ -1,44 +1,143 @@
-const socket = io.connect('http://127.0.0.1:4302');
+const socket = io('http://127.0.0.1:4302', { autoConnect: false });
 
 const message = document.getElementById('messages');
 const send = document.getElementById('btn');
 
 const commands = [1, 99, 98, 97, 0];
+let currentOrder;
 
-socket.on('welcome', (prompts) => {
-  for (let prompt of prompts) {
-    message.innerHTML += `<p>${prompt.msg}</p>`;
-  }
-  for (let cmd of commands) {
-    message.innerHTML += `<button onclick="handleClick(this)">${cmd}</button>`;
-  }
-});
-
-const handleClick = (e) => {
-  message.innerHTML += `<p>${e.innerText}</p>`;
-  socket.emit(e.innerText, e.innerText);
+const connectChat = (username) => {
+  socket.auth = { username };
+  socket.connect();
 };
 
-socket.on('1', (data) => {
-  message.innerHTML += `<p>${data.msg}</p>`;
-  for (let i in data.foods) {
-    message.innerHTML += `<button onclick="handleClick(this)" value="${parseInt(i) + 10}">${data.foods[i]}</button>`;
+// form submission for user creation
+document.forms['new-user'].addEventListener('submit', (e) => {
+  e.preventDefault();
+  fetch(e.target.action, {
+    method: 'POST',
+    body: new URLSearchParams(new FormData(e.target)),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      document.forms['new-user'].style.display = 'none';
+      document.getElementById('main').style.display = 'block';
+      connectChat(document.getElementById('username').value);
+    })
+    .catch((error) => {
+      document.getElementById('err').innerHTML = 'Username already exist';
+      document.getElementById('err').style.color = 'red';
+    });
+});
 
+socket.on('welcome', (prompts) => {
+  message.innerHTML += `<p class="sender">Hello and Welcome to SeeFood, the best place for your Hotdog and Not Hotdog meals.</p>`;
+  message.appendChild(handlePrompt('div', prompts));
+  message.appendChild(handlePrompt('button', commands));
+});
+
+socket.on('1', (foods) => {
+  message.innerHTML += `<p class="sender">Select from our wide range of delicacies</p>`;
+  message.appendChild(handlePrompt('button', foods, true));
+});
+
+socket.on('99', ({ prompts, lastOrder }) => {
+  message.innerHTML += `<p class="sender">Congratulations, Your order ${lastOrder.food} with the id ${lastOrder.id} have been ${lastOrder.status}</p>`;
+  message.appendChild(handlePrompt('div', prompts));
+  message.appendChild(handlePrompt('button', commands));
+});
+
+socket.on('98', ({ prompts, history }) => {
+  message.innerHTML += `<p class="sender">Order History</p>`;
+  if (history[0]) {
+    for (let order of history) {
+      message.innerHTML += `<p class="sender">At ${new Date(
+        order.date
+      ).toUTCString()} order ${order.food} with ${order.id} was ${
+        order.status
+      } </p>`;
+    }
+  } else {
+    message.innerHTML += `<p class="sender">You have not created any order.</p>`;
   }
+  message.appendChild(handlePrompt('div', prompts));
+  message.appendChild(handlePrompt('button', commands));
 });
 
-socket.on('99', (data) => {
-  message.innerHTML += `<p>${data}</p>`;
+socket.on('97', ({ lastOrder, prompts }) => {
+  if (lastOrder) {
+    message.innerHTML += `<p class="sender">Your last order was ${lastOrder.food} with the id ${lastOrder.id}. The status is ${lastOrder.status}</p>`;
+  } else {
+    message.innerHTML += `<p class="sender">You have not created any order.</p>`;
+  }
+  message.appendChild(handlePrompt('div', prompts));
+  message.appendChild(handlePrompt('button', commands));
 });
 
-socket.on('98', (data) => {
-  message.innerHTML += `<p>${data}</p>`;
+socket.on('0', ({ prompts, lastOrder }) => {
+  message.innerHTML += `<p class="sender">Your order ${lastOrder.food} with the id ${lastOrder.id} have been ${lastOrder.status}</p>`;
+  message.appendChild(handlePrompt('div', prompts));
+  message.appendChild(handlePrompt('button', commands));
 });
 
-socket.on('97', (data) => {
-  message.innerHTML += `<p>${data}</p>`;
+socket.on('chat', (data) => {
+  message.innerHTML += `<p class="sender">${data.msg}</p>`;
+  message.appendChild(handlePrompt('div', data.prompt));
+  message.appendChild(handlePrompt('button', [99, 0]));
 });
 
-socket.on('0', (data) => {
-  message.innerHTML += `<p>${data}</p>`;
-});
+// handle bot prompts 
+const handlePrompt = (elem, commands, val) => {
+  let div = document.createElement('div');
+  div.setAttribute('class', 'prompt-container');
+
+  if (elem === 'button' && !val) {
+    for (let cmd of commands) {
+      let element = document.createElement(elem);
+      element.innerHTML += cmd;
+      element.setAttribute('class', 'prompt-btn');
+      element.setAttribute('onclick', 'handleClick(this)');
+      div.appendChild(element);
+    }
+    return div;
+  }
+  if (elem === 'div') {
+    for (let prompt of commands) {
+      let element = document.createElement('p');
+      element.innerHTML += prompt.msg;
+      div.appendChild(element);
+      div.setAttribute('class', 'prompt-container');
+    }
+    return div;
+  }
+
+  for (let i in commands) {
+    let element = document.createElement(elem);
+    element.innerHTML += commands[i];
+    element.setAttribute('class', 'prompt-btn');
+    element.setAttribute('onclick', 'handleClick(this)');
+    element.setAttribute('value', parseInt(i) + 10);
+    div.appendChild(element);
+  }
+  return div;
+};
+
+// handle button clicks 
+const handleClick = (e) => {
+  //   message.scrollTo(0, message.scrollHeight + 100);
+  message.scrollTop = message.scrollHeight;
+  message.innerHTML += `<p class="receiver">${e.innerText}</p>`;
+  if (e.value) {
+    currentOrder = e.innerText;
+    socket.emit('food', e.innerText);
+  } else {
+    if (e.innerText === '99' && !currentOrder) {
+      message.innerHTML += `<p class="sender">Sorry you do not have any active order to checkout</p>`;
+      return;
+    }
+    socket.emit(e.innerText, e.innerText);
+  }
+};
+ 
--- src/app.js
+++ src/app.js
@@ -7,49 +7,109 @@ const app = new express();
 const server = http.createServer(app);
 const io = new Socket(server);
 
+
+let users = [];
+let lastOrder;
 const prompts = [
-  {
-    msg: 'Hello and Welcome to SeeFood, the best place for your Hotdog and Not Hotdog meals.',
-  },
-  { msg: 'How can we help you today?' },
   { msg: 'Select 1 to place an order.' },
   { msg: 'Select 99 to checkout your order.' },
   { msg: 'Select 98 to see your order history.' },
   { msg: 'Select 97 to see your current order' },
   { msg: 'Select 0 to cancel your order.' },
 ];
+const foods = ['Hotdog', 'Pizza', 'Calamari', 'French Fires', 'chicken'];
 
-const response = {
-  placeOrder: {
-    msg: 'Select from our wide range of delicacies',
-    foods: ['Hotdog', 'Pizza', 'Calamari', 'French Fires', 'chicken'],
-  },
-};
+app
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .use(express.static('public'))
+  .get('/', (req, res) => {
+    res.status(200);
+  })
+  .post('/', (req, res) => {
+    const { name, username } = req.body;
+    if (users.filter((item) => item.username === username).length !== 0) {
+      res.status(401).end('already exist');
+      return;
+    }
+    users.push({ name, username });
+    res.status(201).end('completed');
+    console.log(users);
+  });
 
-app.use(express.static('public')).get('/', (req, res) => {
-  res.status(200);
+io.use((socket, next) => {
+  const username = socket.handshake.auth.username;
+  if (!username) {
+    return next(new Error('invalid username'));
+  }
+  socket.username = username;
+  next();
 });
 
 io.on('connection', (socket) => {
+  let idx = users.findIndex((item) => item.username === socket.username);
+  users[idx].userId = socket.id;
+  users[idx].history = [];
   socket.emit('welcome', prompts);
 
   socket.on('1', (data) => {
-    socket.emit(data, response.placeOrder);
+    console.log(foods);
+    socket.emit(data, foods);
   });
   socket.on('99', (data) => {
-    socket.emit(data, 'Congratulations, Your order have been completed');
+    lastOrder = users[idx].history[users[idx].history.length - 1];
+    users[idx].history.push({
+      ...lastOrder,
+      date: Date.now(),
+      status: 'Confirmed',
+    });
+    lastOrder = users[idx].history[users[idx].history.length - 1];
+    socket.emit(data, { lastOrder, prompts });
+    // socket.emit('welcome', prompts);
+    console.log(users[0].history);
   });
 
   socket.on('98', (data) => {
-    socket.emit(data, 'Here are your order history');
+    socket.emit(data, { history: users[idx].history, prompts });
   });
 
   socket.on('97', (data) => {
-    socket.emit(data, 'Your current order');
+    socket.emit(data, { lastOrder, prompts });
   });
 
   socket.on('0', (data) => {
-    socket.emit(data, 'Cancellation Successful!');
+    lastOrder = users[idx].history[users[idx].history.length - 1];
+    users[idx].history.push({
+      ...lastOrder,
+      date: Date.now(),
+      status: 'Cancelled',
+    });
+    lastOrder = users[idx].history[users[idx].history.length - 1];
+    socket.emit(data, { prompts, lastOrder });
+  });
+
+  socket.on('food', (data) => {
+    let id = Math.floor(Math.random() * 1000) + 1;
+    users[idx].history.push({
+      id,
+      date: Date.now(),
+      food: data,
+      status: 'Opened',
+    });
+    socket.emit('chat', {
+      msg: `Your order ${data} with the id of ${
+        users[idx].history[users[idx].history.length - 1].id
+      } have been created.`,
+      prompt: [
+        { msg: 'Select 99 to checkout your order.' },
+        { msg: 'Select 0 to cancel your order.' },
+      ],
+    });
+  });
+
+  socket.on('disconnect', () => {
+    users.splice(users.findIndex(id => id === socket.id))
+    console.log('User left');
   });
 });
 
